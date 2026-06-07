a8eba00029cb104a0df7cfdd5cabac619ca9f9fd
sfk-aayan <190041116faiyaz@gmail.com>
2026-06-08 01:24:54 +0600

    feat: files updated for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
+    base: "/aayan-portfolio/",
+
     plugins: [react(), tailwindcss()],
+
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
       },
     },
+
     server: {
       hmr: process.env.DISABLE_HMR !== "true",
       watch: process.env.DISABLE_HMR === "true" ? null : {},
